10b699eb5a15f5f5433ea59974873989dd4da22e
anisoara1 <todea.anisoara@gmail.com>
2024-12-18 22:07:36 +0200

    menu cards

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import config from "../config";
 import "./Hero.css";
 import backgroundImage from "../assets/fresh-baked-donuts.jpg";
 import burger from "../assets/burger.jpg";
+import vector from "../assets/Vector 1.png";
 
 const Hero = () => (
   <section className="hero">
@@ -15,11 +16,12 @@ const Hero = () => (
       </div>
     </div>
     <div className="image-container">
+      <img src={vector} alt="vector" className="vector" />
       <div className="first-image">
         <img src={backgroundImage} alt="Fresh baked donuts" />
       </div>
       <div className="second-image">
-        <img src={burger} alt="Fresh baked donuts" />
+        <img src={burger} alt="Burger" />
       </div>
     </div>
   </section>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,3 @@
-// src/components/Menu.js
 import React from "react";
 import config from "../config";
 import "./Menu.css";
@@ -6,31 +5,64 @@ import "./Menu.css";
 const menuItems = [
   {
     name: "Gogoși cu ciocolată",
-    description: "Delicioase gogoși umplute cu ciocolată.",
+    description:
+      "Delicioase gogoși umplute cu ciocolată.Delicioase gogoși umplute cu ciocolată.Delicioase gogoși umplute cu ciocolată.",
     price: "10 lei",
+    image: "path/to/chocolate-donut.jpg",
   },
   {
     name: "Gogoși cu gem",
     description: "Gogoși pufoase cu gem de fructe.",
     price: "8 lei",
+    image: "path/to/jam-donut.jpg",
   },
   {
     name: "Gogoși simple",
     description: "Gogoși clasice, perfecte pentru orice ocazie.",
     price: "6 lei",
+    image: "path/to/plain-donut.jpg",
   },
-  // Adaugă mai multe elemente de meniu după cum este necesar
+  {
+    name: "Gogoși cu ciocolată",
+    description:
+      "Delicioase gogoși umplute cu ciocolată.Delicioase gogoși umplute cu ciocolată.",
+    price: "10 lei",
+    image: "path/to/chocolate-donut.jpg",
+  },
+  {
+    name: "Gogoși cu gem",
+    description: "Gogoși pufoase cu gem de fructe.",
+    price: "8 lei",
+    image: "path/to/jam-donut.jpg",
+  },
+  {
+    name: "Gogoși simple",
+    description:
+      "Gogoși clasice, perfecte pentru orice ocazie.Delicioase gogoși umplute cu ciocolată.Delicioase gogoși umplute cu ciocolată.",
+    price: "6 lei",
+    image: "path/to/plain-donut.jpg",
+  },
 ];
 
 const Menu = () => (
   <section className="menu">
     <h2>{config.menuTitle}</h2>
     <div className="menu-container">
+      <div className="vertical-border"></div>
       {menuItems.map((item, index) => (
         <div key={index} className="menu-item">
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          <p className="price">{item.price}</p>
+          <div className="image-card">
+            <img src={item.image} alt={item.name} className="menu-image" />
+          </div>
+          <div className="text-container">
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <hr className="orange-line" />
+            <div className="price-container">
+              <button className="order-btn">Comandă acum</button>
+              <h4 className="price">{item.price}</h4>
+            </div>
+          </div>
         </div>
       ))}
     </div>
