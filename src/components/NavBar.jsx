95e27dc5f225227be2fd0d9c198163c54e1947f2
anisoara1 <todea.anisoara@gmail.com>
2024-12-29 01:52:34 +0200

    changed styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,6 +14,9 @@ export const NavBar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const isAdminPage =
+    location.pathname === "/login" || location.pathname === "/admin";
+
   return (
     <header className="header">
       <div className="container">
@@ -39,27 +42,50 @@ export const NavBar = () => {
             </li>
           </ul>
         </nav>
-        <button className="book-table-button">Cosul tau</button>
-        <ul className="admin-link">
-          {location.pathname === "/login" ? (
-            <li>
-              <Link to="/atelier">Home</Link>
-            </li>
-          ) : (
-            !isAuthenticated && (
+        <button className="book-table-button">
+          <span className="cart-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </span>
+          <span className="cart-text">Cosul tau</span>
+        </button>
+
+        {isAdminPage && (
+          <ul className="admin-link">
+            {location.pathname === "/login" ? (
               <li>
-                <Link to="/login">Admin</Link>
+                <Link to="/atelier">Home</Link>
               </li>
-            )
-          )}
-          {isAuthenticated && location.pathname !== "/login" && (
-            <li>
-              <Link to={location.pathname === "/admin" ? "/atelier" : "/admin"}>
-                {location.pathname === "/admin" ? "Home" : "Admin"}
-              </Link>
-            </li>
-          )}
-        </ul>
+            ) : (
+              !isAuthenticated && (
+                <li>
+                  <Link to="/login">Admin</Link>
+                </li>
+              )
+            )}
+            {isAuthenticated && location.pathname !== "/login" && (
+              <li>
+                <Link
+                  to={location.pathname === "/admin" ? "/atelier" : "/admin"}
+                >
+                  {location.pathname === "/admin" ? "Home" : "Admin"}
+                </Link>
+              </li>
+            )}
+          </ul>
+        )}
         <button className="burger-menu" onClick={toggleMenu}>
           ☰
         </button>
@@ -91,30 +117,36 @@ export const NavBar = () => {
                   Contact
                 </a>
               </li>
-              {location.pathname === "/login" ? (
-                <li>
-                  <Link to="/atelier" onClick={toggleMenu}>
-                    Home
-                  </Link>
-                </li>
-              ) : (
-                !isAuthenticated && (
-                  <li>
-                    <Link to="/login" onClick={toggleMenu}>
-                      Admin
-                    </Link>
-                  </li>
-                )
-              )}
-              {isAuthenticated && location.pathname !== "/login" && (
-                <li>
-                  <Link
-                    to={location.pathname === "/admin" ? "/atelier" : "/admin"}
-                    onClick={toggleMenu}
-                  >
-                    {location.pathname === "/admin" ? "Home" : "Admin"}
-                  </Link>
-                </li>
+              {isAdminPage && (
+                <>
+                  {location.pathname === "/login" ? (
+                    <li>
+                      <Link to="/atelier" onClick={toggleMenu}>
+                        Home
+                      </Link>
+                    </li>
+                  ) : (
+                    !isAuthenticated && (
+                      <li>
+                        <Link to="/login" onClick={toggleMenu}>
+                          Admin
+                        </Link>
+                      </li>
+                    )
+                  )}
+                  {isAuthenticated && location.pathname !== "/login" && (
+                    <li>
+                      <Link
+                        to={
+                          location.pathname === "/admin" ? "/atelier" : "/admin"
+                        }
+                        onClick={toggleMenu}
+                      >
+                        {location.pathname === "/admin" ? "Home" : "Admin"}
+                      </Link>
+                    </li>
+                  )}
+                </>
               )}
             </ul>
           </div>
